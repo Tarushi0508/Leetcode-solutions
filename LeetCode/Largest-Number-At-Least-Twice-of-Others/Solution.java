57bcab4e3f04834e25abf2dba48783f32a279a02
Tarushi Purwar <tarushipurwar4@gmail.com>
2025-10-28 18:47:21 +0530

    Add solution for Largest Number At Least Twice of Others

---
+++ LeetCode/Largest-Number-At-Least-Twice-of-Others/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[max]) max=i;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[max]<2*nums[i] && i!=max)
+            return -1;
+        }
+        return max;
+    }
+}
